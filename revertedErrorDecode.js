73012d024651ee1bbd0a8a04f942ee4d222b6313
Lucas Coutinho <91095004+lucasdcoutinh0@users.noreply.github.com>
2022-06-03 11:05:17 -0300

    Update revertedErrorDecode.js

--- revertedErrorDecode.js
+++ revertedErrorDecode.js
@@ -1,5 +1,5 @@
 const { ethers } = require("ethers");
-let provider = new ethers.providers.InfuraProvider("maticmum", "d13778b232614309af22fda20a18a3af");
+let provider = new ethers.providers.InfuraProvider("maticmum", "Your Infura Project Id");
 
 
 async function revertedErrorDecode(){
