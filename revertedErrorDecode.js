e5c5bbb3615d826ffdedce36d39949d1458d7a13
Lucas Coutinho <lucas.coutinho@eagronom.com>
2022-06-03 10:31:48 -0300

    update

--- revertedErrorDecode.js
+++ revertedErrorDecode.js
@@ -3,8 +3,6 @@ let provider = new ethers.providers.InfuraProvider("maticmum", "d13778b232614309
 
 
 async function revertedErrorDecode(){
-    const txHash = "0x6d30217834ee538b5a99aad005a45ced8a22edd723542c741fb590ec33f12da6"
-    const reciept = await provider.getTransactionReceipt(txHash)
     try{
         const reciept = await provider.getTransaction('0x6d30217834ee538b5a99aad005a45ced8a22edd723542c741fb590ec33f12da6');
         let code = await provider.call(reciept, reciept.blockNumber)
